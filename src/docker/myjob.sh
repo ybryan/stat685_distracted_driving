8c41a332d27c15cdeaae860664ae179a942cd634
Bryan Yu <bryan.yu@santecapital.com>
2018-08-26 15:21:14 -0500

    Move to env var

--- src/docker/myjob.sh
+++ src/docker/myjob.sh
@@ -5,11 +5,12 @@ echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
 echo "resultKey: $RESULT_KEY"
 echo "hashValue: $HASH_VALUE"
+echo "rScriptFile: $RSCRIPT_FILE"
 echo "stanFile: $STAN_FILE"
 
-Rscript --vanilla $STAN_FILE $BATCH_FILE_S3_URL
+Rscript --vanilla $RSCRIPT_FILE $BATCH_FILE_S3_URL $STAN_FILE
 
 ls -l
-aws s3 cp $RESULT_KEY s3://stat685-batch/results/$HASH_VALUE/
+aws s3 cp $RESULT_KEY s3://stat685-batch/results/$STAN_FILE/$HASH_VALUE/
 
 echo "bye bye!!"
