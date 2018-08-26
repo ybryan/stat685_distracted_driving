d3076ab3fa974abd0bbab3b63cc0b8cb7447e830
Bryan Yu <bryan.yu@santecapital.com>
2018-08-26 12:27:58 -0500

    Run with 8 schools success

--- src/docker/8schools.R
+++ src/docker/8schools.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 library("rstan") 
-library("aws.s3")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
@@ -16,18 +15,17 @@ source_file_name <- tail(strsplit(args[1], "/")[[1]], n=1)
 hash_run_number  <- strsplit(source_file_name, "[.]")[[1]][1]
 hash             <- strsplit(hash_run_number, "_")[[1]][1]
 run_number       <- strsplit(hash_run_number, "_")[[1]][2]
-new_results      <- paste(hash_run_number, ".rds", sep="")
-key <- paste("results", hash, new_results, sep="/")
+new_results      <- paste("results/", hash, "/", hash_run_number, ".Rdata", sep="")
 
-if (!object_exists(key, bucket_name)){
-    # Run STAN
-    schools_dat <- list(J = 8,
-                        y = c(28,  8, -3,  7, -1,  1, 18, 12),
-                        sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
-    fit <- stan(file = '8schools.stan', data = schools_dat,
-                iter = 1000, chains = 4)
-    print(fit, digits = 1)
+# Run STAN
+schools_dat <- list(J = 8,
+                    y = c(28,  8, -3,  7, -1,  1, 18, 12),
+                    sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+fit <- stan(file = '8schools.stan', data = schools_dat,
+            iter = 1000, chains = 4)
+print(fit, digits = 1)
 
-    # Save file
-    s3saveRDS(fit, object=key, bucket=bucket_name)
-}
+# Save file
+print(new_results)
+saveRDS(fit, file=paste(hash_run_number, ".Rdata", sep=""))
+
--- src/docker/myjob.sh
+++ src/docker/myjob.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 date
-
 echo "jobId: $AWS_BATCH_JOB_ID"
 echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
+echo "resultKey: $RESULT_KEY"
+echo "hashValue: $HASH_VALUE"
 
 Rscript --vanilla 8schools.R $BATCH_FILE_S3_URL
 
-date
+ls -l
+aws s3 cp $RESULT_KEY s3://stat685-batch/results/$HASH_VALUE/
+
 echo "bye bye!!"
