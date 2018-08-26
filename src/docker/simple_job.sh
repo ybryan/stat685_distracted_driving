8be3f2d71631202cdf0e772d23282edffc73d7dd
Bryan Yu <bryan.yu@santecapital.com>
2018-08-25 22:14:21 -0500

    Batch test works

--- src/docker/simple_job.sh
+++ src/docker/simple_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 date
-echo "This is my simple test job!."
+Rscript 8schools.R
+
 echo "jobId: $AWS_BATCH_JOB_ID"
 echo "jobQueue: $AWS_BATCH_JQ_NAME"
 echo "computeEnvironment: $AWS_BATCH_CE_NAME"
