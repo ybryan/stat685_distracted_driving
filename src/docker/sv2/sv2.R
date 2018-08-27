7fe852cbf139e9a1778a9a7fa4b99a083fbe545e
Bryan Yu <ybryan@gmail.com>
2018-08-27 05:27:42 +0000

    Output only!

--- src/docker/sv2/sv2.R
+++ src/docker/sv2/sv2.R
@@ -19,11 +19,60 @@ new_results      <- paste("results/", hash, "/", hash_run_number, ".Rdata", sep=
 
 # Run STAN
 load("simu_data.Rdata")
-fit <- stan(file = args[2], data = simu_data,
-            iter = 1000, chains = 4)
+fit <- stan(file = args[2], data = simu_data, seed=4938483)
 print(fit, digits = 1)
 
-# Save file
-print(new_results)
-saveRDS(fit, file=paste(hash_run_number, ".Rdata", sep=""))
+# Run diagnostics
+util <- new.env()
+source('stan_utility.R', local=util)
 
+warning_code <- util$check_all_diagnostics(fit, quiet=TRUE)
+
+# Compute rank of prior draw with respect to thinned posterior draws
+sbc_rank_mu <- sum(simu_mu < extract(fit)$mu[seq(1, 4000 - 8, 8)])
+sbc_rank_phi <- sum(simu_phi < extract(fit)$phi[seq(1, 4000 - 8, 8)])
+sbc_rank_sigma <- sum(simu_sigma < extract(fit)$sigma[seq(1, 4000 - 8, 8)])
+sbc_rank_delta <- sum(simu_delta < extract(fit)$delta_texting[seq(1, 4000 - 8, 8)])
+
+# Compute posterior sensitivities
+s <- summary(fit, probs = c(), pars='mu')$summary
+post_mean_mu <- s[,1]
+post_sd_mu <- s[,3]
+prior_sd_mu <- 1
+z_score_mu <- abs((post_mean_mu - simu_mu) / post_sd_mu)
+shrinkage_mu <- 1 - (post_sd_mu / prior_sd_mu)**2
+
+s <- summary(fit, probs = c(), pars='phi')$summary
+post_mean_phi <- s[,1]
+post_sd_phi <- s[,3]
+prior_sd_phi <- sqrt(1/12 * (1 - -1)^2)
+z_score_phi <- abs((post_mean_phi - simu_phi) / post_sd_phi)
+shrinkage_phi <- 1 - (post_sd_phi / prior_sd_phi)**2
+
+s <- summary(fit, probs = c(), pars='sigma')$summary
+post_mean_sigma <- s[,1]
+post_sd_sigma <- s[,3]
+prior_sd_sigma <- 1 * (1 - 2 / pi)
+z_score_sigma <- abs((post_mean_sigma- simu_sigma) / post_sd_sigma)
+shrinkage_sigma <- 1 - (post_sd_sigma / prior_sd_sigma)**2
+
+s <- summary(fit, probs = c(), pars='delta_texting')$summary
+post_mean_delta <- s[,1]
+post_sd_delta <- s[,3]
+prior_sd_delta <- 0.05
+z_score_delta <- abs((post_mean_delta - simu_delta) / post_sd_delta)
+shrinkage_delta <- 1 - (post_sd_delta/ prior_sd_delta)**2
+
+output <- c(warning_code,
+  sbc_rank_mu, z_score_mu, shrinkage_mu,
+  sbc_rank_phi, z_score_phi, shrinkage_phi,
+  sbc_rank_sigma, z_score_sigma, shrinkage_sigma,
+  sbc_rank_delta, z_score_delta, shrinkage_delta)
+print(c("warning_code", 
+        "sbc_rank_mu",  "z_score_mu",  "shrinkage_mu",
+        "sbc_rank_phi", "z_score_phi", "shrinkage_phi",
+        "sbc_rank_sigma", "z_score_sigma", "shrinkage_sigma",
+        "sbc_rank_delta", "z_score_delta", "shrinkage_delta"))
+print(output)
+# Save file
+saveRDS(output, file=paste(hash_run_number, ".Rdata", sep=""))
